4829b492feeeaef8ba5ca3331626ed1bf63890de
dove <dove@feegr.cc>
2024-01-16 06:17:58 +0000

    split index into computation and page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -17,19 +17,31 @@ export default async function Page() {
                 <MyCard
                     value={missions.totalNum}
                     maxValue={totalMissionNum}
-                    color="success"
+                    color={
+                        missions.totalNum / totalMissionNum > 0.5 ?
+                            "success" :
+                            "warning"
+                    }
                     cardfoot={`通话个数${missions.totalNum} / ${totalMissionNum} 个`}
                 />
                 <MyCard
                     value={Math.floor(missions.totalTime / 60)}
                     maxValue={totalMissionMinutes}
-                    color="success"
+                    color={
+                        Math.floor(missions.totalTime / 60) / missions.totalNum > 0.5 ?
+                            "success" :
+                            "warning"
+                    }
                     cardfoot={`今日时长 ${Math.floor(missions.totalTime / 60)} / ${totalMissionMinutes} 分钟`}
                 />
                 <MyCard
                     value={missions.totalAnswered}
                     maxValue={missions.totalNum}
-                    color="success"
+                    color={
+                        missions.totalAnswered / missions.totalNum > 0.5 ?
+                            "success" :
+                            "warning"
+                    }
                     cardfoot="接通率"
                 />
             </div>
